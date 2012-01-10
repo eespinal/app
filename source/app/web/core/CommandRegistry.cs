21522d91ead3f3bd1db07effe1f427eebe92f46e
Emilio <esantelises@intellisys.com.do>
2012-01-10 18:55:00 -0400

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -6,15 +6,19 @@ namespace app.web.core
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessASingleRequest> all_commands;
+      private IProcessASingleRequest missing_command;
 
-    public CommandRegistry(IEnumerable<IProcessASingleRequest> all_commands)
-    {
-      this.all_commands = all_commands;
-    }
+      public CommandRegistry(IProcessASingleRequest missingCommand, IEnumerable<IProcessASingleRequest> allCommands)
+      {
+          missing_command = missingCommand;
+          all_commands = allCommands;
+      }
 
-    public IProcessASingleRequest get_the_command_that_can_process(IProvideDetailsToCommands the_request)
+      public IProcessASingleRequest get_the_command_that_can_process(IProvideDetailsToCommands the_request)
     {
-      return all_commands.First(x => x.can_process(the_request));
+          var command_can_proccess_the_request = all_commands.FirstOrDefault(x => x.can_process(the_request));
+
+          return command_can_proccess_the_request ?? missing_command; 
     }
   }
 }
