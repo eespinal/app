0648539984b2bd741171106d3d38190964df76c2
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-01-10 14:07:42 -0700

    Updated

--- build/support/002_configuration/git.rb
+++ build/support/002_configuration/git.rb
@@ -1,8 +1,9 @@
 configs ={
   :git => {
     :user => "20120109intellisys",
-    :remotes => potentially_change("remotes",__FILE__),
+    :remotes => %w[sramirez4 jorfx jzabala augustointellisys carlosjln kakusio jeortiz lalmanzar esantelises eespinal josercruz01 manuelcerda rlantigua jdelacruz josue812 raydelto],
     :repo => 'app' 
   }
 }
 configatron.configure_from_hash(configs)
+
