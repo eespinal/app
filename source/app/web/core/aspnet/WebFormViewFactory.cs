268fce855a659b165f027d4e54a62e1f0b8114f6
juan <jortiz@intellisys.com.do>
2012-01-11 14:56:42 -0400

    Pushing new changes

--- source/app/web/core/aspnet/WebFormViewFactory.cs
+++ source/app/web/core/aspnet/WebFormViewFactory.cs
@@ -1,20 +1,22 @@
 ﻿using System.Web;
+using System.Web.UI;
 
 namespace app.web.core.aspnet
 {
   public class WebFormViewFactory : ICreateViews
   {
     IFindPathsToTemplates template_path_registry;
+      private readonly WebFormFactory webFormFactory;
 
-    public WebFormViewFactory(IFindPathsToTemplates template_path_registry)
+      public WebFormViewFactory(IFindPathsToTemplates template_path_registry, WebFormFactory webFormFactory)
     {
-      this.template_path_registry = template_path_registry;
+        this.template_path_registry = template_path_registry;
+        this.webFormFactory = webFormFactory;
     }
 
-    public IHttpHandler create_view_that_can_render<ReportModel>(ReportModel report)
+      public IHttpHandler create_view_that_can_render<ReportModel>(ReportModel report)
     {
-      template_path_registry.get_path_to_template_for<ReportModel>();
-      return null;
+      return webFormFactory(template_path_registry.get_path_to_template_for<ReportModel>(),typeof(Page)) as IHttpHandler;
     }
   }
 }
