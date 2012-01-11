e4e75b04f8b35f32af88d69dacdb6d2f4e1cc172
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-01-11 11:13:47 -0700

    Updated

--- source/app/web/core/stubs/StubRequestFactory.cs
+++ source/app/web/core/stubs/StubRequestFactory.cs
@@ -1,9 +1,7 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Web;
 using app.web.application.catalogbrowing;
-using app.web.application.catalogbrowing.stubs;
 
 namespace app.web.core.stubs
 {
@@ -18,21 +16,7 @@ namespace app.web.core.stubs
     {
       public InputModel map<InputModel>()
       {
-        object item;
-        var rand = (new Random()).Next(0, 4);
-        switch (rand)
-        {
-          case 0:
-            item = new Department(){Products = new List<Product>(){new Product(),new Product()}};
-            break;
-          case 1:
-            item = new Department() {SubDepartments= new List<Department>() { new Department(), new Department() } };
-            break;
-          default:
-            item = new Department();
-          break;
-        }
-        return (InputModel) item;
+        throw new NotImplementedException();
       }
     }
   }
