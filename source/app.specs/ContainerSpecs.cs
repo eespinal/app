24330074b70f9e4d091959bfaa70bbf2db33a61a
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-01-11 15:29:47 -0700

    Updated

--- source/app.specs/ContainerSpecs.cs
+++ source/app.specs/ContainerSpecs.cs
@@ -1,8 +1,6 @@
 ﻿using Machine.Specifications;
-using Machine.Specifications.Runner.Impl;
 using app.utility.containers;
 using developwithpassion.specifications.rhinomocks;
-using developwithpassion.specifications.extensions;
 
 namespace app.specs
 {
@@ -11,24 +9,22 @@ namespace app.specs
   {
     public abstract class concern : Observes
     {
-
     }
 
     public class when_providing_access_to_the_container_facade : concern
     {
       Establish c = () =>
       {
-        ContainerFacadeResolver resolver = () => the_facade;
         the_facade = fake.an<IFetchDependencies>();
+        ContainerFacadeResolver resolver = () => the_facade;
         spec.change(() => Container.facade_resolver).to(resolver);
-
       };
+
       Because b = () =>
         result = Container.fetch;
 
       It should_return_the_container_facade_configured_at_startup = () =>
         result.ShouldEqual(the_facade);
-
 
       static IFetchDependencies result;
       static IFetchDependencies the_facade;
