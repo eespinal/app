dc80a5198e1b6e2759710af01ff4348aa55e26e9
Jorge Ferreira <jferreira@intellisys.com.do>
2012-01-11 19:11:11 -0400

    Pushing new changes

--- source/app/utility/containers/DependencyContainer.cs
+++ source/app/utility/containers/DependencyContainer.cs
@@ -5,15 +5,28 @@ namespace app.utility.containers
   public class DependencyContainer : IFetchDependencies
   {
       readonly IFindFactoriesForDependencies find_factories_for_dependencies;
+      readonly ItemCreationExceptionFactory item_creation_exception_factory;
+      Type dependency_type;
 
-      public DependencyContainer(IFindFactoriesForDependencies find_factories_for_dependencies)
+      public DependencyContainer(IFindFactoriesForDependencies find_factories_for_dependencies, ItemCreationExceptionFactory item_creation_exception_factory)
       {
           this.find_factories_for_dependencies = find_factories_for_dependencies;
+          this.item_creation_exception_factory = item_creation_exception_factory;
       }
 
       public Dependency an<Dependency>()
       {
-          return (Dependency) find_factories_for_dependencies.get_factory_that_can_create(typeof (Dependency)).create();
+          dependency_type = typeof(Dependency);
+          try
+          {
+              return (Dependency)find_factories_for_dependencies.get_factory_that_can_create(dependency_type).create();
+          }
+          catch (Exception ex)
+          {
+
+              throw item_creation_exception_factory(dependency_type, ex);
+
+          }
       }
   }
 }
