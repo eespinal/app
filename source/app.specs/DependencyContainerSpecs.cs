b5536cee9d5da6ff8fc79fc4e654bf1f345a5904
Jorge Ferreira <jferreira@intellisys.com.do>
2012-01-11 18:45:46 -0400

    Pushing new changes

--- source/app.specs/DependencyContainerSpecs.cs
+++ source/app.specs/DependencyContainerSpecs.cs
@@ -1,33 +1,34 @@
-﻿ using Machine.Specifications;
- using app.utility.containers;
- using developwithpassion.specifications.rhinomocks;
- using developwithpassion.specifications.extensions;
+﻿using Machine.Specifications;
+using app.utility.containers;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
 
 namespace app.specs
-{  
-  [Subject(typeof(DependencyContainer))]  
-  public class DependencyContainerSpecs
-  {
-    public abstract class concern : Observes<IFetchDependencies,
-                                      DependencyContainer>
+{
+    [Subject(typeof (DependencyContainer))]
+    public class DependencyContainerSpecs
     {
-        
-    }
+        public abstract class concern : Observes<IFetchDependencies,
+                                            DependencyContainer>
+        {
+        }
 
-   
-    public class when_getting_a_dependency : concern
-    {
-        Because of = () => result = sut.an<IStubDependency>();
-                         
-        It should_return_a_valid_instance = () => result.ShouldBeAn<IStubDependency>();
 
-        static IStubDependency result;
-        
-    }
+        public class when_getting_a_dependency : concern
+        {
+            Because of = () => result = sut.an<IStubDependency>();
 
-    public interface IStubDependency
-    {
-         
+            It should_return_a_valid_instance = () => result.ShouldBeAn<StubDependency>();
+
+            static IStubDependency result;
+        }
+
+        public interface IStubDependency
+        {
+        }
+
+        public class StubDependency : IStubDependency
+        {
+        }
     }
-  }
 }
