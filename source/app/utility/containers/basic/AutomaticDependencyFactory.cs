3d354913f09b3f322acf3861975fca2e5b4898ed
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-01-12 11:47:02 -0700

    Updated

--- source/app/utility/containers/basic/AutomaticDependencyFactory.cs
+++ source/app/utility/containers/basic/AutomaticDependencyFactory.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
 using app.utility.containers.core;
 
 namespace app.utility.containers.basic
 {
   public class AutomaticDependencyFactory : ICreateOneType
   {
-     IFetchDependencies container;
-     IPickTheCtorOnTheType constructor_picker;
-     Type type_that_i_create;
+    IFetchDependencies container;
+    IPickTheCtorOnTheType constructor_picker;
+    Type type_that_i_create;
 
     public AutomaticDependencyFactory(IFetchDependencies container, IPickTheCtorOnTheType constructor_picker,
                                       Type type_that_i_create)
@@ -22,11 +21,8 @@ namespace app.utility.containers.basic
     public object create()
     {
       var constructor = constructor_picker.get_applicable_ctor_on(type_that_i_create);
-
-      return Activator.CreateInstance(type_that_i_create,
-                                      constructor.GetParameters().Select(
-                                        parameter =>
-                                          container.an(parameter.ParameterType)).ToArray());
+      var items = constructor.GetParameters().Select(x => container.an(x.ParameterType));
+      return constructor.Invoke(items.ToArray());
     }
   }
 }
