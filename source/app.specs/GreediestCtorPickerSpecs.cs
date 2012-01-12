3d0e77c0469d9a4253ad4f97332973dc3f625ffb
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-01-12 11:10:57 -0700

    Updated

---
+++ source/app.specs/GreediestCtorPickerSpecs.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Machine.Specifications;
+using app.specs.utility;
+using app.utility.containers.core;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+  [Subject(typeof(GreediestCtorPicker))]
+  public class GreediestCtorPickerSpecs
+  {
+    public abstract class concern : Observes<IPickTheCtorOnTheType,
+                                      GreediestCtorPicker>
+    {
+    }
+
+    public class when_getting_the_ctor_for_a_type : concern
+    {
+      Establish c = () =>
+      {
+        greediest_ctor = ObjectFactory.expressions.to_target<AutomaticDependencyFactorySpecs.OurTypeWithDependencies>()
+          .get_ctor_pointed_at_by(() => new AutomaticDependencyFactorySpecs.OurTypeWithDependencies(null, null, null));
+      };
+
+      Because b = () =>
+        result = sut.get_applicable_ctor_on(typeof(AutomaticDependencyFactorySpecs.OurTypeWithDependencies));
+
+      It should_return_the_greediest = () =>
+        result.ShouldEqual(greediest_ctor);
+
+      static ConstructorInfo result;
+      static ConstructorInfo greediest_ctor;
+    }
+  }
+}
---
+++ source/app/utility/containers/core/GreediestCtorPicker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace app.utility.containers.core
+{
+  public class GreediestCtorPicker : IPickTheCtorOnTheType
+  {
+    public ConstructorInfo get_applicable_ctor_on(Type type)
+    {
+      return type.GetConstructors().OrderByDescending(x => x.GetParameters().Count()).First();
+    }
+  }
+}
